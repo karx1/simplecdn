f3096f28b86306ded94a0ab2b0bb33d7a326a1fc
Yash Karandikar <nerdstep710@gmail.com>
2020-10-23 11:21:30 -0500

    Rewrite env check logic in rust

---
+++ check-env/src/lib.rs
@@ -0,0 +1,40 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use pyo3::exceptions::PyValueError;
+use std::env;
+
+struct PyVarError {
+    msg: String
+}
+
+impl std::convert::From<PyVarError> for PyErr {
+    fn from(err: PyVarError) -> PyErr {
+        PyValueError::new_err(err.to_string())
+    }
+}
+
+
+impl std::fmt::Display for PyVarError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "VarError: {}", self.msg)
+    }
+}
+
+fn get_env_var() -> Result<String, PyVarError> {
+    match env::var("DATA_DIR") {
+        Ok(val) => Ok(val.to_string()),
+        Err(e) => Err(PyVarError{ msg: e.to_string() })
+    }
+}
+
+#[pyfunction]
+fn get_data_dir() -> PyResult<String> {
+    Ok(get_env_var()?)
+}
+
+#[pymodule]
+fn check_env(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_data_dir, m)?)?;
+
+    Ok(())
+}
